bf612d32d89006d94665ecea385bfebe8aea5d1f
Ben Perlmutter <benperlmutter94@gmail.com>
2020-04-25 17:28:36 -0500

    fix bug for bitmoji on about page

--- assets/js/random-bitmoji.js
+++ assets/js/random-bitmoji.js
@@ -1,18 +1,11 @@
 export default function randomBitmoji(){
-    const h1Wrap = document.querySelector(".h1-wrap");
     let randomBitmojiNumber = Math.floor(Math.random() * 16 + 1);
     randomBitmojiNumber = randomBitmojiNumber < 10 ? "0" + randomBitmojiNumber : randomBitmojiNumber;
     const filePath = `/assets/img/random-bitmoji/${randomBitmojiNumber}.png`;
     if(document.querySelector(".h1-bitmoji")){
         document.querySelector(".h1-bitmoji").src = filePath;
     }
-// else{
-//         const img = document.createElement("img");
-//         img.classList.add("h1-bitmoji");
-//         img.src = filePath;
-//         img.addEventListener("click", randomBitmoji);
-//         h1Wrap.prepend(img);
-//     }
+
 }
 
 
